368127c97514c299c74915ca1f8d0d971a1eee65
Mauro Lucero <mauroaglucero@gmail.com>
2024-08-13 16:29:04 -0300

    Update head.php

--- components/head.php
+++ components/head.php
@@ -5,6 +5,15 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content='"Clinicas ABM" es un proyecto que recrea un sistema para gestionar varias clínicas, áreas médicas, especialidades de médicos, pacientes y consultas realizadas por los pacientes. Aún se encuentra en desarrollo.'>
+
+    <!-- Open Graph -->
+    <meta property="og:title" content="Clinic System - Gestión de clínicas, hospitales y centros médicos">
+    <meta property="og:type" content="app">
+    <meta property="og:description" content='"Clinicas ABM" es un proyecto que recrea un sistema para gestionar varias clínicas, áreas médicas, especialidades de médicos, pacientes y consultas realizadas por los pacientes. Aún se encuentra en desarrollo.'>
+    <meta property="og:url" content="https://www.maurolucero.com.ar/projects/clinic-system">
+    <meta property="og:image" content="https://www.maurolucero.com.ar/img/banner.webp">
+    <meta property="og:locale" content="es_ES">
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -13,7 +22,8 @@
     <link rel="stylesheet" href="css/general.css">
 
     <!-- Titulo -->
-    <title>Clínicas ABM</title>
+    <link rel="icon" type="image/webp" href="https://www.maurolucero.com.ar/img/icon-64.webp">
+    <title>Clinic System - Gestión de clínicas, hospitales y centros médicos</title>
 </head>
 
 <body>
